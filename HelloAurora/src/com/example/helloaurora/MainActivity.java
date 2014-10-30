5d75dd0aedb5df91103822e331c59c212c456671
gkalic <kalic.goran@gmail.com>
2014-10-30 22:27:19 +0100

    gkalic

--- HelloAurora/src/com/example/helloaurora/MainActivity.java
+++ HelloAurora/src/com/example/helloaurora/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-// komentar bzivkovic
+// komentar bzivkovic i gkalic
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
